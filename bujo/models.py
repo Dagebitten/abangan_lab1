2a70f2bce45c4c0baf193f3a0f62e3add453b5b4
Marc <gabriel.abangan@obf.ateneo.edu>
2021-04-10 18:44:22 +0800

    Fixed Profile Page
    Profile Page Image now works. Made database into postgreSQL and other related changed

--- bujo/models.py
+++ bujo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_profilePicture =  models.ImageField(default='default.jpg',upload_to='profile_pics')
+    user_profileBio = models.CharField(max_length=500)
+    user_keys = models.CharField(max_length=100)
+    user_weekTasks  = models.CharField(max_length=200)
+    user_todayTasks = models.CharField(max_length=200)
+
 # Create your models here.
